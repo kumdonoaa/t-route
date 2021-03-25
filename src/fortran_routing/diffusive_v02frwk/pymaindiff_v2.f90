15b1694f1c003334a61e2b1eebd917527e294d2d
awlostowski-noaa <adam.wlostowski@noaa.gov>
2021-03-25 07:47:22 -0400

    minor changes

--- src/fortran_routing/diffusive_v02frwk/pymaindiff_v2.f90
+++ src/fortran_routing/diffusive_v02frwk/pymaindiff_v2.f90
@@ -33,7 +33,7 @@ subroutine c_diffnw(dtini_g, t0_g, tfin_g, saveinterval_g, saveinterval_ev_g, dt
     real(c_double), intent(in) :: so_llm_g
     integer(c_int), intent(in) :: ntss_ev_g
     real(c_double), dimension(ntss_ev_g, mxncomp_g, nrch_g), intent(out) :: q_ev_g, elv_ev_g
-
+    
     call diffnw(dtini_g, t0_g, tfin_g, saveinterval_g, saveinterval_ev_g, dt_ql_g, dt_ub_g,&
         dt_db_g, nts_ql_g, nts_ub_g, nts_db_g, mxncomp_g, nrch_g, z_ar_g, bo_ar_g, traps_ar_g,&
         tw_ar_g, twcc_ar_g, mann_ar_g, manncc_ar_g, so_ar_g, dx_ar_g, nhincr_m_g, nhincr_f_g,&
