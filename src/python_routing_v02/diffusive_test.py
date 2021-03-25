4257a6ab0f6e3fbaf361d7f34ef275498fee8fb6
awlostowski-noaa <adam.wlostowski@noaa.gov>
2021-03-25 14:46:42 -0400

    using real model input data from yaml - Florence test

--- src/python_routing_v02/diffusive_test.py
+++ src/python_routing_v02/diffusive_test.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pathlib
 import sys
+import yaml
+import os
 
 ENV_IS_CL = False
 if ENV_IS_CL:
@@ -10,98 +12,53 @@ elif not ENV_IS_CL:
     sys.path.append("fast_reach")
 
 ## network and reach utilities
-import troute.nhd_network_utilities_v02 as nnu
 import diffusive
 
-# variables needed to run diffusive model
-dtini_g = 1.0  #inital time interval [secs]
-t0_g = 0.0 #simulation starting time [hrs]
-tfin_g = 1.0 #simulation ending time [hrs]
-saveinterval_g = 1.0 #output publishing time interval [secs]
-saveinterval_ev_g = saveinterval_g
-
-dt_ql_g = 1.0 #time interval of wrf-hydro qlat input data [sec]
-dt_ub_g = 1.0 #time interval of input data for upstream boundary condition [sec]
-dt_db_g = 1.0 #time interval of input data for downstream boundary condition [sec]
-            
-            
-nts_ql_g = 1 #number of simulation timesteps per qlateral time interval [-]
-nts_ub_g = 1 #number of simulation timesteps per upstream boundary time interval [-]
-nts_db_g = 1 #number of simulation timesteps per downstream boundary time interval [-]
-
-mxncomp_g = 4 #maximum number of nodes in a reach
-nrch_g = 6 #number network reaches
-
-z_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #altitude of each segment of each reach
-bo_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #channel bottom width
-traps_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #side slope of main channel bank
-tw_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #top width of main channel
-twcc_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #top width of the floodplain
-mann_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #manning's n for main channel
-manncc_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #manning's n for floodplain
-so_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #channel bottom slope
-dx_ar_g = np.empty([mxncomp_g,nrch_g], dtype = np.double) #segment length
-
-nhincr_m_g = 4 # number of water table elevation increments used for making uniform flow lookup table for MC
-nhincr_f_g = 4 # number of water table elevation increments used for making uniform flow lookup table for floodplain
-ufhlt_m_g = np.empty([mxncomp_g, nrch_g, nhincr_m_g], dtype = np.double) #water elevation array for uniform flow lookup table
-ufqlt_m_g = np.empty([mxncomp_g, nrch_g, nhincr_m_g], dtype = np.double) #discharge array for uniform flow lookup table
-ufhlt_f_g = np.empty([mxncomp_g, nrch_g, nhincr_f_g], dtype = np.double) #water elevation array for uniform flow lookup table
-ufqlt_f_g = np.empty([mxncomp_g, nrch_g, nhincr_f_g], dtype = np.double) #water elevation array for uniform flow lookup table
-
-frnw_col = 4 #numer of columns in fortran network map
-frnw_g = np.empty([nrch_g, frnw_col], dtype = 'int32') # Fortran network map
-
-
-qlat_g = np.empty([nts_ql_g, mxncomp_g, nrch_g], dtype = np.double) 
-ubcd_g = np.empty([nts_ub_g, nrch_g], dtype = np.double) 
-dbcd_g = np.empty([nts_ub_g], dtype = np.double) 
-
-cfl_g = 0.98
-theta_g = 0.25
-tzeq_flag_g = 1
-y_opt_g = 0
-so_llm_g = 0.00001
-ntss_ev_g = 10
-
-out_q, out_elv = diffusive.compute_diffusive(dtini_g,
-                                t0_g,
-                                tfin_g,
-                                saveinterval_g,
-                                saveinterval_ev_g,
-                                dt_ql_g,
-                                dt_ub_g,
-                                dt_db_g,
-                                nts_ql_g,
-                                nts_ub_g,
-                                nts_db_g,
-                                mxncomp_g,
-                                nrch_g,
-                                np.asfortranarray(z_ar_g),
-                                np.asfortranarray(bo_ar_g),
-                                np.asfortranarray(traps_ar_g),
-                                np.asfortranarray(tw_ar_g),
-                                np.asfortranarray(twcc_ar_g),
-                                np.asfortranarray(mann_ar_g),
-                                np.asfortranarray(manncc_ar_g),
-                                np.asfortranarray(so_ar_g),
-                                np.asfortranarray(dx_ar_g),
-                                nhincr_m_g,
-                                nhincr_f_g,
-                                np.asfortranarray(ufhlt_m_g),
-                                np.asfortranarray(ufqlt_m_g),
-                                np.asfortranarray(ufhlt_f_g),
-                                np.asfortranarray(ufqlt_f_g),
-                                frnw_col,
-                                np.asfortranarray(frnw_g),
-                                np.asfortranarray(qlat_g),
-                                np.asfortranarray(ubcd_g),
-                                np.asfortranarray(dbcd_g),
-                                cfl_g,
-                                theta_g,
-                                tzeq_flag_g,
-                                y_opt_g,
-                                so_llm_g,
-                                ntss_ev_g,
+# open input data from yaml
+with open(os.path.join(root,'src/python_routing_v02_diffusive/diff_inputs.yml')) as file:
+    data_list = yaml.load(file, Loader=yaml.Loader)
+    
+print(data_list["frnw_g"])
+    
+out_q, out_elv = diffusive.compute_diffusive(data_list["dtini_g"],
+                                data_list["t0_g"],
+                                data_list["tfin_g"],
+                                data_list["saveinterval_g"],
+                                data_list["saveinterval_ev_g"],
+                                data_list["dt_ql_g"],
+                                data_list["dt_ub_g"],
+                                data_list["dt_db_g"],
+                                data_list["nts_ql_g"],
+                                data_list["nts_ub_g"],
+                                data_list["nts_db_g"],
+                                data_list["mxncomp_g"],
+                                data_list["nrch_g"],
+                                np.asfortranarray(data_list["z_ar_g"]),
+                                np.asfortranarray(data_list["bo_ar_g"]),
+                                np.asfortranarray(data_list["traps_ar_g"]),
+                                np.asfortranarray(data_list["tw_ar_g"]),
+                                np.asfortranarray(data_list["twcc_ar_g"]),
+                                np.asfortranarray(data_list["mann_ar_g"]),
+                                np.asfortranarray(data_list["manncc_ar_g"]),
+                                np.asfortranarray(data_list["so_ar_g"]),
+                                np.asfortranarray(data_list["dx_ar_g"]),
+                                data_list["nhincr_m_g"],
+                                data_list["nhincr_f_g"],
+                                np.asfortranarray(data_list["ufhlt_m_g"]),
+                                np.asfortranarray(data_list["ufqlt_m_g"]),
+                                np.asfortranarray(data_list["ufhlt_f_g"]),
+                                np.asfortranarray(data_list["ufqlt_f_g"]),
+                                data_list["frnw_col"],
+                                np.asfortranarray(data_list["frnw_g"].astype('double')),
+                                np.asfortranarray(data_list["qlat_g"]),
+                                np.asfortranarray(data_list["ubcd_g"]),
+                                np.asfortranarray(data_list["dbcd_g"]),
+                                data_list["cfl_g"],
+                                data_list["theta_g"],
+                                data_list["tzeq_flag_g"],
+                                data_list["y_opt_g"],
+                                data_list["so_llm_g"],
+                                data_list["ntss_ev_g"],
                                )
 
+print(out_elv)
