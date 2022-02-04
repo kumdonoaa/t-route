0c3fcc34089b70a8bdb549be2b3187f2fa1ff262
awlostowski-noaa <adamw@cheyenne6.cheyenne.ucar.edu>
2022-02-04 16:13:14 -0700

    make sure the topobahy dataframe is populated before we try to index it, else an error will be thrown

--- src/python_routing_v02/troute/routing/compute.py
+++ src/python_routing_v02/troute/routing/compute.py
@@ -974,8 +974,11 @@ def compute_diffusive_routing(
         # create DataFrame of junction inflow data            
         junction_inflows = pd.DataFrame(data = trib_flow, index = trib_segs)
         
-        # create topobathy data for diffusive mainstem segments related to this given tw segment        
-        topobathy_data_bytw  = topobathy_data.loc[diffusive_network_data[tw]['mainstem_segs']] 
+        if not topobathy_data.empty:
+            # create topobathy data for diffusive mainstem segments related to this given tw segment        
+            topobathy_data_bytw  = topobathy_data.loc[diffusive_network_data[tw]['mainstem_segs']] 
+        else:
+            topobathy_data_bytw = None
 
         # build diffusive inputs
         diffusive_inputs = diff_utils.diffusive_input_data_v02(
